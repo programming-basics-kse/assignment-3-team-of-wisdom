a826cc8a14f211af14c9a57771a57d44a98529da
ZakharGuzii <zhuzii@kse.org.ua>
2024-11-16 22:00:09 +0200

    add medals part

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ parser.add_argument("data_file", type=str, help="Address of the file with data")
 subparsers = parser.add_subparsers(dest="command", required=True, help="Commands")
 
 # Підкоманда -medals
+medals_parser = subparsers.add_parser("medals", help="Analyze medals for a specific country and year")
+medals_parser.add_argument("country", type=str, help="Country name or NOC code (e.g., 'USA' or 'United States')")
+medals_parser.add_argument("year", type=int, help="Year of the Olympic Games")
+medals_parser.add_argument("-output", type=str, help="Optional file to save the results")
+args = parser.parse_args()
