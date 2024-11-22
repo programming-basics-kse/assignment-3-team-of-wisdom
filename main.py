ebda522475b638519dadc7c1496316e30af9977f
ivanlukianets <ilukianets@kse.org.ua>
2024-11-22 14:04:50 +0200

    Creating function medals. 3 validation there and main logic

--- main.py
+++ main.py
@@ -32,8 +32,41 @@ def read_data(file_path):
             data.append(dict(zip(header, values)))
     return data
 
-def medals_command(data):
-    pass
+def medals_command(data,country,year,output_file = "",):
+    ten_athlete_medalist = {}
+    valid_year_counter =0 # Validation for year
+
+    for row in data:
+        if year == int(row["Year"]):
+            valid_year_counter +=1
+    if valid_year_counter == 0:
+        return "Sorry, You have entered inappropriate year value you should try again!"
+
+    valid_country_counter = 0 # Validation for country
+
+    for row in data:
+        if row["Team"].lower() == country.lower() or row["NOC"].lower() == country.lower():
+            valid_country_counter +=1
+    if valid_country_counter == 0:
+        return "Sorry, You have entered inappropriate country value you should try again!"
+
+    medalist_counter =0
+
+    for row in data:
+        if (int(row["Year"]) == year) and (row["Team"].lower() == country.lower() or row["NOC"].lower() == country.lower()) and (row['Medal'] != 'NA'):
+            ten_athlete_medalist[row["Name"]] = [row["Sport"],row["Medal"]]
+            medalist_counter +=1
+        if medalist_counter == 10:
+            break
+    if medalist_counter < 10: # Validation if there are less than 10 medalists
+        return f"Sorry, This country have less than 10 athletes with medals. It only has {medalist_counter} athletes "
+
+    return ten_athlete_medalist
+
+    # Make more readable displace of command result and integrating of output file through library pickle
+
+
+
 
 def total_command(data, year):
     country_medals = {}
@@ -48,11 +81,12 @@ def total_command(data, year):
         result.append(f"{country} - {medals['Gold']} - {medals['Silver']} - {medals['Bronze']}")
     return "\n".join(result)
 
-while True:
-    args = parser.parse_args()
-    if args.command == "medals":
-        print(medals_command(read_data(args.data_file)))
-    elif args.command == "total":
-        print(total_command(read_data(args.data_file),args.year))
+
+
+if args.command == "medals":
+    print(medals_command(read_data(args.data_file),args.country,args.year,args.output))
+elif args.command == "total":
+    print(total_command(read_data(args.data_file),args.year))
+
 
 
