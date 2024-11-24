eee448b1a69a3b54d46cc0d0010d4b3d1651007b
ivanlukianets <ilukianets@kse.org.ua>
2024-11-25 00:11:49 +0200

    Finally completing function medals_command and adding right_data.tsv

--- main.py
+++ main.py
@@ -34,38 +34,54 @@ def read_data(file_path):
 
 def medals_command(data,country,year,output_file = "",):
     ten_athlete_medalist = {}
-    valid_year_counter =0 # Validation for year
 
+    valid_year_counter =0 # Validation for year
     for row in data:
         if year == int(row["Year"]):
             valid_year_counter +=1
     if valid_year_counter == 0:
-        return "Sorry, You have entered inappropriate year value you should try again!"
+         return print("Sorry, You have entered inappropriate year value you should try again!")
 
     valid_country_counter = 0 # Validation for country
-
     for row in data:
         if row["Team"].lower() == country.lower() or row["NOC"].lower() == country.lower():
             valid_country_counter +=1
     if valid_country_counter == 0:
-        return "Sorry, You have entered inappropriate country value you should try again!"
+        return print("Sorry, You have entered inappropriate country value you should try again!")
 
     medalist_counter =0
-
-    for row in data:
+    for row in data: # Searching for 10 medalists
         if (int(row["Year"]) == year) and (row["Team"].lower() == country.lower() or row["NOC"].lower() == country.lower()) and (row['Medal'] != 'NA'):
             ten_athlete_medalist[row["Name"]] = [row["Sport"],row["Medal"]]
             medalist_counter +=1
         if medalist_counter == 10:
             break
     if medalist_counter < 10: # Validation if there are less than 10 medalists
-        return f"Sorry, This country have less than 10 athletes with medals. It only has {medalist_counter} athletes "
+        return print(f"Sorry, This country have less than 10 athletes with medals. It only has {medalist_counter} athletes ")
 
-    return ten_athlete_medalist
+    country_medals = {}
+    for row in data: # Summing up information about country medals
+        if (int(row["Year"]) == year) and (row["Team"].lower() == country.lower() or row["NOC"].lower() == country.lower()) and (row['Medal'] != 'NA'):
+            country_in_loop = row["Team"]
+            if country_in_loop not in country_medals:
+                country_medals[country_in_loop] = {"Gold": 0, "Silver": 0, "Bronze": 0}
+            country_medals[country_in_loop][row['Medal']] += 1
 
-    # Make more readable displace of command result and integrating of output file through library pickle
+    for name,sport_medal in ten_athlete_medalist.items(): # Printing result of function
+        print(f"{name} - {sport_medal[0]} - {sport_medal[1]}")
+    print(country_medals)
 
-
+    if output_file is None:
+        pass
+    elif output_file == "":
+        pass
+    else:
+        with open(output_file,"w") as file:
+            for name, sport_medal in ten_athlete_medalist.items():
+                file.write(f"{name} - {sport_medal[0]} - {sport_medal[1]}\n")
+            for country, medals in sorted(country_medals.items()):
+                file.write(f"{country} - {medals['Gold']} - {medals['Silver']} - {medals['Bronze']}\n")
+    return ten_athlete_medalist,country_medals
 
 
 def total_command(data, year):
@@ -78,13 +94,12 @@ def total_command(data, year):
             country_medals[country][row['Medal']] += 1
     result = []
     for country, medals in sorted(country_medals.items()):
-        result.append(f"{country} - {medals['Gold']} - {medals['Silver']} - {medals['Bronze']}")
+        result.append(f"{country} - Gold: {medals['Gold']} - Silver: {medals['Silver']} - Bronze: {medals['Bronze']}")
     return "\n".join(result)
 
 
-
 if args.command == "medals":
-    print(medals_command(read_data(args.data_file),args.country,args.year,args.output))
+    medals_command(read_data(args.data_file),args.country,args.year,args.output)
 elif args.command == "total":
     print(total_command(read_data(args.data_file),args.year))
 
